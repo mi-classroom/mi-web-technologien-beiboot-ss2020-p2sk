5303ba127280340a13fc537999727dd39f147a9d
p2sk <pascal.schorde@gmail.com>
2020-08-11 16:24:43 +0200

    add typescript file + transpiled

---
+++ frontend/public/js/script.js
@@ -0,0 +1,4 @@
+var message = document.createTextNode('Hello from Typescript! Its beeing watched.');
+var messageNode = document.createElement('p');
+messageNode.appendChild(message);
+document.body.appendChild(messageNode);
---
+++ frontend/src/script.ts
@@ -0,0 +1,5 @@
+const message = document.createTextNode('Hello from Typescript! Its beeing watched.')
+const messageNode = document.createElement('p')
+messageNode.appendChild(message)
+
+document.body.appendChild(messageNode)
