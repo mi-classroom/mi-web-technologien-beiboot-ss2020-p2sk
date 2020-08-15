ca7a6ef33c732d59b031cad426825e335d6861eb
p2sk <pascal.schorde@gmail.com>
2020-08-15 13:35:45 +0200

    ts and js files

--- frontend/public/js/script.js
+++ frontend/public/js/script.js
@@ -1,4 +1,13 @@
-var message = document.createTextNode('Hello from Typescript! Its beeing watched.');
-var messageNode = document.createElement('p');
-messageNode.appendChild(message);
-document.body.appendChild(messageNode);
+var backendUri = 'http://localhost:8080/rest/v1/collections';
+var collectionRequest = new Request(backendUri);
+var handleJsonData = function (data) {
+    var outputElement = document.getElementById('json-output');
+    outputElement.append(JSON.stringify(data));
+    /* Handle the Json data */
+    console.log(data);
+};
+fetch(collectionRequest)
+    .then(function (res) { return res.json(); })
+    .then(handleJsonData)["catch"](function (error) {
+    console.error('Error:', error);
+});
--- frontend/src/script.ts
+++ frontend/src/script.ts
@@ -1,5 +1,19 @@
-const message = document.createTextNode('Hello from Typescript! Its beeing watched.')
-const messageNode = document.createElement('p')
-messageNode.appendChild(message)
 
-document.body.appendChild(messageNode)
+const backendUri = 'http://localhost:8080/rest/v1/collections'
+
+let collectionRequest = new Request(backendUri)
+
+let handleJsonData = function(data) {
+    let outputElement = document.getElementById('json-output')
+
+    outputElement.append(JSON.stringify(data))
+    /* Handle the Json data */
+    console.log(data)
+}
+
+fetch(collectionRequest) 
+.then(res => res.json())
+.then(handleJsonData)
+.catch(error => {
+    console.error('Error:', error)
+})
