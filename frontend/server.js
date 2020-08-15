e0a41d7e37dd7961afc2b0b99d1087d3a97e66d5
p2sk <pascal.schorde@gmail.com>
2020-08-15 13:34:57 +0200

    frontend server

--- frontend/server.js
+++ frontend/server.js
@@ -3,17 +3,16 @@ nodejs server für das frontend
 served via basic http
 */
 
-var fs = require('fs'),
-    http = require('http'),
-    static = require('node-static');
+var http = require('http')
+var static = require('node-static')
 
-const hostname = '0.0.0.0';
-const port = 8081;
+const hostname = '0.0.0.0'
+const port = 8081
 
-var file = new static.Server('./public');
+var file = new static.Server('./public')
 
 http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
-}).listen(port, hostname);
+  request.addListener('end', function () {
+    file.serve(request, response)
+  }).resume()
+}).listen(port, hostname)
